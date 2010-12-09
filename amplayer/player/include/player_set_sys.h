3dfa4299971809b9d21198da8ec5a016be83032e
adam.chen <adam.chen@amlogic.com>
2010-12-09 20:48:44 +0800

    internel subtitle control

--- amplayer/player/include/player_set_sys.h
+++ amplayer/player/include/player_set_sys.h
@@ -6,15 +6,7 @@ int get_black_policy();
 int set_tsync_enable(int enable);
 int set_fb0_blank(int blank);
 int set_fb1_blank(int blank);
-int set_subtitle_enable(int enable);
-int get_subtitle_enable();
 int set_subtitle_num(int num);
-int get_subtitle_num();
-int set_subtitle_curr(int curr);
-int get_subtitle_curr();
-int set_subtitle_size(int size);
-int get_subtitle_size();
-int set_subtitle_data(int data);
-int get_subtitle_data();
+int av_get_subtitle_curr();
 
 #endif
--- amplayer/player/player_set_sys.c
+++ amplayer/player/player_set_sys.c
@@ -61,40 +61,6 @@ int set_tsync_enable(int enable)
     
 }
 
-int set_subtitle_enable(int enable)
-{
-    int fd;
-    char *path = "/sys/class/subtitle/enable";    
-	char  bcmd[16];
-	fd=open(path, O_CREAT|O_RDWR | O_TRUNC, 0644);
-	if(fd>=0)
-	{
-    	sprintf(bcmd,"%d",enable);
-    	write(fd,bcmd,strlen(bcmd));
-    	close(fd);
-    	return 0;
-	}
-	return -1;
-    
-}
-
-int get_subtitle_enable()
-{
-    int fd;
-	int subtitle_enable = 0;
-    char *path = "/sys/class/subtitle/enable";    
-	char  bcmd[16];
-	fd=open(path, O_RDONLY);
-	if(fd>=0)
-	{    	
-    	read(fd,bcmd,sizeof(bcmd));       
-        subtitle_enable = strtol(bcmd, NULL, 16);       
-        subtitle_enable &= 0x1;
-    	close(fd);    	
-	}
-	return subtitle_enable;   
-}
-
 
 int set_subtitle_num(int num)
 {
@@ -113,42 +79,7 @@ int set_subtitle_num(int num)
     
 }
 
-int get_subtitle_num()
-{
-    int fd;
-	int subtitle_num = 0;
-    char *path = "/sys/class/subtitle/total";    
-	char  bcmd[16];
-	fd=open(path, O_RDONLY);
-	if(fd>=0)
-	{    	
-    	read(fd,bcmd,sizeof(bcmd));       
-        subtitle_num = strtol(bcmd, NULL, 16);       
-        subtitle_num &= 0x1;
-    	close(fd);    	
-	}
-	return subtitle_num;   
-}
-
-
-int set_subtitle_curr(int curr)
-{
-    int fd;
-    char *path = "/sys/class/subtitle/curr";    
-	char  bcmd[16];
-	fd=open(path, O_CREAT|O_RDWR | O_TRUNC, 0644);
-	if(fd>=0)
-	{
-    	sprintf(bcmd,"%d",curr);
-    	write(fd,bcmd,strlen(bcmd));
-    	close(fd);
-    	return 0;
-	}
-	return -1;
-    
-}
-
-int get_subtitle_curr()
+int av_get_subtitle_curr()
 {
     int fd;
 	int subtitle_cur = 0;
@@ -157,80 +88,11 @@ int get_subtitle_curr()
 	fd=open(path, O_RDONLY);
 	if(fd>=0)
 	{    	
-    	read(fd,bcmd,sizeof(bcmd));       
-        subtitle_cur = strtol(bcmd, NULL, 16);       
-        subtitle_cur &= 0x1;
+    	read(fd,bcmd,sizeof(bcmd)); 
+		sscanf(bcmd, "%d", &subtitle_cur);
     	close(fd);    	
 	}
 	return subtitle_cur;   
-}
-
-int set_subtitle_size(int size)
-{
-    int fd;
-    char *path = "/sys/class/subtitle/size";    
-	char  bcmd[16];
-	fd=open(path, O_CREAT|O_RDWR | O_TRUNC, 0644);
-	if(fd>=0)
-	{
-    	sprintf(bcmd,"%d",size);
-    	write(fd,bcmd,strlen(bcmd));
-    	close(fd);
-    	return 0;
-	}
-	return -1;
-    
-}
-
-int get_subtitle_size()
-{
-    int fd;
-	int subtitle_size = 0;
-    char *path = "/sys/class/subtitle/size";    
-	char  bcmd[16];
-	fd=open(path, O_RDONLY);
-	if(fd>=0)
-	{    	
-    	read(fd,bcmd,sizeof(bcmd));       
-        subtitle_size = strtol(bcmd, NULL, 16);       
-        //subtitle_size &= 0x1;
-    	close(fd);    	
-	}
-	return subtitle_size;   
-}
-
-int set_subtitle_data(int data)
-{
-    int fd;
-    char *path = "/sys/class/subtitle/data";    
-	char  bcmd[16];
-	fd=open(path, O_CREAT|O_RDWR | O_TRUNC, 0644);
-	if(fd>=0)
-	{
-    	sprintf(bcmd,"%d",data);
-    	write(fd,bcmd,strlen(bcmd));
-    	close(fd);
-    	return 0;
-	}
-	return -1;
-    
-}
-
-int get_subtitle_data()
-{
-    int fd;
-	int subtitle_data = 0;
-    char *path = "/sys/class/subtitle/data";    
-	char  bcmd[16];
-	fd=open(path, O_RDONLY);
-	if(fd>=0)
-	{    	
-    	read(fd,bcmd,sizeof(bcmd));       
-        subtitle_data = strtol(bcmd, NULL, 16);       
-        //subtitle_cur &= 0x1;
-    	close(fd);    	
-	}
-	return subtitle_data;   
 }
 
 int set_fb0_blank(int blank)
