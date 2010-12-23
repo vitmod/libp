a0d5ca7f9d7a55e6e7e87691cd44ff4bb198773f
adam.chen <adam.chen@amlogic.com>
2010-12-23 13:51:03 +0800

    remove subtitle, now int SubTitle git

--- amcodec/Android.mk
+++ amcodec/Android.mk
@@ -5,11 +5,9 @@ LOCAL_SRC_FILES := \
 	codec/codec_ctrl.c \
 	codec/codec_h_ctrl.c \
 	codec/codec_msg.c \
-	audio_ctl/audio_ctrl.c \
-	subtitle/subtitle.c \
-	subtitle/vob_sub.c
+	audio_ctl/audio_ctrl.c
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/codec $(LOCAL_PATH)/audio_ctl $(LOCAL_PATH)/subtitle $(LOCAL_PATH)/../amadec/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/codec $(LOCAL_PATH)/audio_ctl$(LOCAL_PATH)/../amadec/include
 LOCAL_ARM_MODE := arm
 LOCAL_STATIC_LIBRARIES := libamadec
 LOCAL_MODULE:= libamcodec
