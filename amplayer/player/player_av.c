e8d156d2da2cb212fb26974d33d17c20d612bc01
jeff.yang <jeff.yang@amlogic.com>
2010-12-24 18:35:12 +0800

    seperate mkv graphic type from vobsub

--- amplayer/player/player_av.c
+++ amplayer/player/player_av.c
@@ -1589,6 +1589,11 @@ int process_es_subtitle(play_para_t *para, am_packet_t *pkt)
 	} 
     if (!para->sstream_info.check_first_pts)
         para->sstream_info.check_first_pts = 1;
+        
+    if(sub_type==0x17000)
+    {
+    	sub_type=0x1700a;
+    }
 
     /* first write the header */
     sub_type = para->sstream_info.sub_type;
