8be7715b8455d1aebfd3b0d0aaff90db04a0e8fb
Lee.Rong <lee.rong@amlogic.com>
2011-02-15 14:51:37 +0800

    Optimized format script

--- tools/format.sh
+++ tools/format.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
-#usage: ./format file
-astyle --style=linux --suffix=.orig1 $@ 
-astyle --indent=spaces=4 --suffix=.orig2 $@
+#Format the source code(s)
+astyle --style=linux --indent=spaces=4 --brackets=linux --indent-col1-comments --pad-oper --pad-header \
+       --unpad-paren --add-brackets --convert-tabs --mode=c --lineend=linux $@ >/tmp/astyle.log
+
+#Deal with the log of astyle 
+sed -i -e 's;formatted  ;;g' /tmp/astyle.log;
+sed -i -e 's;unchanged\* ;;g' /tmp/astyle.log;
+
+#Define global variable
+DIR=$PWD
+#Get the directory of the source code(s) to generate the patch
+line=`sed -n 1p /tmp/astyle.log`
+DIR=`dirname $line`
+
+#The needn;t /tmp/astyle.log any more, delete it.
+rm -rf /tmp/astyle.log
+
+#Check if the patch file existed
+if [ -e ${DIR}/linux_style.patch ]; then 
+	rm -rf ${DIR}/linux_style.patch
+fi
+
+#Generate the patch
+suffix=.orig
+list=`find -name "*"${suffix}`
+echo $list
+for i in $list;
+do
+        filename=`echo $i | sed 's/'${suffix}'$//'`;
+        diff -Nura $filename${suffix} $filename >> ${DIR}/linux_style.patch;
+	rm -rf $filename${suffix}
+done
