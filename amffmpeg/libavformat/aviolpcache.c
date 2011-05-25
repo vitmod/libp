04ad5b74dc1242e14936f453ae0977efc6f82093
zhi.zhou <zhi.zhou@amlogic.com>
2011-05-25 10:42:35 +0800

    format cachefile related code only,nochanges.

--- amffmpeg/libavformat/aviolpcache.c
+++ amffmpeg/libavformat/aviolpcache.c
@@ -18,8 +18,8 @@
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
- * 	LOOPBUF READ
- * 	Write By zhi.zhou@amlogic.com
+ *  LOOPBUF READ
+ *  Write By zhi.zhou@amlogic.com
  *
  */
 
@@ -30,66 +30,71 @@
 #include <stdarg.h>
 #include "aviolpcache.h"
 
-static struct cache_client *default_cache_client=NULL;
+static struct cache_client *default_cache_client = NULL;
 
 
 int aviolp_register_cache_system(struct cache_client *cache)
 {
-	if(default_cache_client==NULL)/*only alow first  register*/
-		default_cache_client=cache;
-	else
-		return -1;
-	return 0;
+    if (default_cache_client == NULL) { /*only alow first  register*/
+        default_cache_client = cache;
+    } else {
+        return -1;
+    }
+    return 0;
 }
-static inline struct cache_client   *get_default_cache_client(void)
-{
-	return default_cache_client;
-}
-
-int aviolp_cache_read(int id,int64_t offset,char *buf,int size)
-{
-	struct cache_client *client=get_default_cache_client();
-	if(client && client->cache_read)
-		return client->cache_read(id,offset,buf,size);
-	else
-		return -1;
-}
-int aviolp_cache_write(int id,int64_t offset,char *buf,int size)
-{
-	struct cache_client *client=get_default_cache_client();
-	if(client && client->cache_write)
-		return client->cache_write(id,offset,buf,size);
-	else
-		return -1;
-
+static inline struct cache_client   *get_default_cache_client(void) {
+    return default_cache_client;
 }
 
-int aviolp_cache_open(const char * url,int64_t file_size)
+int aviolp_cache_read(int id, int64_t offset, char *buf, int size)
 {
-	struct cache_client *client=get_default_cache_client();
-	if(client && client->cache_open)
-		return client->cache_open(url,file_size);
-	else
-		return -1;
+    struct cache_client *client = get_default_cache_client();
+    if (client && client->cache_read) {
+        return client->cache_read(id, offset, buf, size);
+    } else {
+        return -1;
+    }
 }
-int aviolp_cache_next_valid_bytes(int id,int64_t offset,int maxsize)
+int aviolp_cache_write(int id, int64_t offset, char *buf, int size)
 {
-	struct cache_client *client=get_default_cache_client();
-	if(client && client->cache_next_valid_bytes)
-		return client->cache_next_valid_bytes(id,offset,maxsize);
-	else
-		return 0;
+    struct cache_client *client = get_default_cache_client();
+    if (client && client->cache_write) {
+        return client->cache_write(id, offset, buf, size);
+    } else {
+        return -1;
+    }
+
+}
+
+int aviolp_cache_open(const char * url, int64_t file_size)
+{
+    struct cache_client *client = get_default_cache_client();
+    if (client && client->cache_open) {
+        return client->cache_open(url, file_size);
+    } else {
+        return -1;
+    }
+}
+int aviolp_cache_next_valid_bytes(int id, int64_t offset, int maxsize)
+{
+    struct cache_client *client = get_default_cache_client();
+    if (client && client->cache_next_valid_bytes) {
+        return client->cache_next_valid_bytes(id, offset, maxsize);
+    } else {
+        return 0;
+    }
 }
 
 
 
 int aviolp_cache_close(int cache_id)
 {
-	struct cache_client *client=get_default_cache_client();
-	if(client && client->cache_close)
-		return client->cache_close(cache_id);
-	else
-		return -1;
+    struct cache_client *client = get_default_cache_client();
+    if (client && client->cache_close) {
+        return client->cache_close(cache_id);
+    } else {
+        return -1;
+    }
 }
 
 
--- amplayer/player/player_cache_mgt.h
+++ amplayer/player/player_cache_mgt.h
@@ -1,7 +1,7 @@
-#ifdef 	PLAYER_CACHE_MGT__
+#ifdef  PLAYER_CACHE_MGT__
 #define PLAYER_CACHE_MGT__
 
-int cache_system_init(int enable,const char*dir,int max_size,int block_size);
+int cache_system_init(int enable, const char*dir, int max_size, int block_size);
 
 
 #endif
