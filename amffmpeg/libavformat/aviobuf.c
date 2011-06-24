42159f23fd82acd886ce608e53af3975cb6442f2
zhi.zhou <zhi.zhou@amlogic.com>
2011-06-24 13:14:45 +0800

    bug on aviobuf may deadlock on read end,

--- amffmpeg/libavformat/aviobuf.c
+++ amffmpeg/libavformat/aviobuf.c
@@ -470,7 +470,7 @@ get_data:
 			if(retry_fill_cnt >= 1000)
 			{
 				av_log(NULL, AV_LOG_ERROR,"[%s]fill buffer retry for a long time,give up!\n",__FUNCTION__);
-				return 0;
+				return URL_EOF;
 			}			 
 			if(s->error == AVERROR(EAGAIN))
 			{
@@ -479,7 +479,7 @@ get_data:
 			} 
         }
     }
-	return 0;
+	return URL_EOF;
 }
 
 int url_fgetc(ByteIOContext *s)
