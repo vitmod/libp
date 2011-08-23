9e835330794d76c219a1f511f3ba9db8d574b2c6
hui.xu <hui.xu@amlogic.com>
2011-08-23 18:52:52 +0800

    if player_sta=error,stoped,playend,exit don't allow get_media_info

--- amplayer/player/player_ctrl.c
+++ amplayer/player/player_ctrl.c
@@ -853,12 +853,18 @@ int player_get_play_info(int pid, player_info_t *info)
 int player_get_media_info(int pid, media_info_t *minfo)
 {
     play_para_t *player_para;
-
+	player_status sta;
+	
     player_para = player_open_pid_data(pid);
     if (player_para == NULL) {
         return PLAYER_NOT_VALID_PID;    /*this data is 0 for default!*/
     }
-
+	
+	sta = get_player_state(player_para);
+	if (sta >= PLAYER_ERROR && sta <= PLAYER_EXIT) {
+		return PLAYER_INVALID_CMD;
+	}
+	
     MEMSET(minfo, 0, sizeof(media_info_t));
     MEMCPY(minfo, &player_para->media_info, sizeof(media_info_t));
 
