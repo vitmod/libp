27d628d34bdcdb046f37d8f63742756728be9b73
hui.xu <hui.xu@amlogic.com>
2011-10-25 15:52:16 +0800

    if seek offset exceed filesize, return error

--- amffmpeg/libavformat/aviobuf.c
+++ amffmpeg/libavformat/aviobuf.c
@@ -202,6 +202,10 @@ int64_t avio_seek(AVIOContext *s, int64_t offset, int whence)
             return offset1;
         offset += offset1;
     }
+	if(offset >= avio_size(s)){
+		av_log(NULL, AV_LOG_INFO, "seek exceed filesize offset:%lld whence=%d, return\n", offset,whence);
+		return AVERROR(EINVAL);
+	}
     offset1 = offset - pos;
     if (!s->must_flush &&
         offset1 >= 0 && offset1 <= (s->buf_end - s->buffer)) {
