b45cc9cb352d6afad31f372202bb430940ee937b
jeff.yang <jeff.yang@amlogic.com>
2012-01-19 10:59:21 +0800

    change about 2XSCALE

--- amavutils/amdisplayutils.c
+++ amavutils/amdisplayutils.c
@@ -12,6 +12,7 @@
 #define FB_DEVICE_PATH   "/sys/class/graphics/fb0/virtual_size"
 #define SCALE_AXIS_PATH  "/sys/class/graphics/fb0/scale_axis"
 #define SCALE_PATH       "/sys/class/graphics/fb0/scale"
+#define SCALE_REQUEST 	 "/sys/class/graphics/fb0/request2XScale"
 #define SYSCMD_BUFSIZE 40
 
 //#define LOG_FUNCTION_NAME LOGI("%s-%d\n",__FUNCTION__,__LINE__);
@@ -52,20 +53,11 @@ int amdisplay_utils_set_scale_mode(int scale_wx, int scale_hx)
 		LOGI("unsupport scaling mode,x1,x2 only\n", scale_wx, scale_hx);
         return -1;
     }
-    ret = amdisplay_utils_get_size(&width, &height);
-    if (ret < 0) {
-        return -1;
-    }
-    neww = width / scale_wx -1;
-    newh = height / scale_hx -1;
-    sprintf(buf, "0 0 %d %d", neww, newh);
-    ret = amsysfs_set_sysfs_str(SCALE_AXIS_PATH, buf);
-    if (ret < 0) {
-        LOGI("set [%s]=[%s] failed\n", SCALE_AXIS_PATH, buf);
-        return -2;
-    }
-    sprintf(buf, "0x%x ", (scale_wx - 1) << 16 | (scale_hx - 1)); /*upper 16 bits is width,low 16 bits is height*/
-    ret = amsysfs_set_sysfs_str(SCALE_PATH, buf);
+	if(scale_wx==2)
+	    ret = amsysfs_set_sysfs_str(SCALE_REQUEST, "1");
+	else if(scale_wx==1)
+	    ret = amsysfs_set_sysfs_str(SCALE_REQUEST, "2");   
+	     
     if (ret < 0) {
         LOGI("set [%s]=[%s] failed\n", SCALE_AXIS_PATH, buf);
         return -2;
