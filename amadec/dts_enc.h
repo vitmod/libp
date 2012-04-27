e58762a2bd038d49e12fab477daea32fbcb04b91
jiwei.sun <jiwei.sun@amlogic.com>
2012-04-27 18:06:37 +0800

    add iec958&pcmenc func to dts_enc

--- amadec/dts_enc.h
+++ amadec/dts_enc.h
@@ -1,22 +1,5 @@
 #ifndef DTS_ENC_H
 #define DTS_ENC_H
-#include <pthread.h>
-
-typedef struct {
-    dtsenc_state_t  state;
-    pthread_t       thread_pid;
-    bool dts_flag;
-}dtsenc_info_t;
-
-typedef enum {
-    IDLE,
-    TERMINATED,
-    STOPPED,
-    INITTED,
-    ACTIVE,
-    PAUSED,
-} dtsenc_state_t;
-
 int dtsenc_init();
 int dtsenc_start();
 int dtsenc_pause();
