eb0fa9fbc1ca32aba27451a562d3075665e3fedc
joy.rao <joy.rao@amlogic.com>
2012-07-25 11:03:51 +0800

    checkin for Yvonne Chen:update duration estimate method for mpegts

--- amffmpeg/libavformat/utils.c
+++ amffmpeg/libavformat/utils.c
@@ -2608,7 +2608,7 @@ static int av_estimate_timings(AVFormatContext *ic, int64_t old_offset)
          !strcmp(ic->iformat->name, "mpegts")) &&
         file_size && ic->pb->seekable) {
         /* get accurate estimate from the PTSes */
-        av_estimate_timings_from_pts(ic, old_offset);
+        av_estimate_timeings_chapters(ic, old_offset);
     } else if (av_has_duration(ic)) {
         /* at least one component has timings - we use them for all
            the components */
