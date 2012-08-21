f614ff4acbed8ffee58ab082c224772701c737a5
xiaoqiang.zhu <xiaoqiang.zhu@amlogic.com>
2012-08-21 16:19:20 +0800

    [HLS]:add api for hls download task,TBD

--- amffmpeg/libavformat/hls_livesession.h
+++ amffmpeg/libavformat/hls_livesession.h
@@ -3,9 +3,13 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-//hSesssion just set NULL,2012,0821
 
+#include"file_list.h"
+
+//hSesssion just set NULL,2012,0821
+list_item_t* getCurrentSegment(void* hSession);
 const char* getCurrentSegmentUrl(void* hSession);
+
 
 #ifdef __cplusplus
 }
