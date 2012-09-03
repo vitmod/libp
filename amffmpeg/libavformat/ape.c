e594d4c63b6c59384718b625e29e2dd7a2fadef8
zhenggang.luo <zhenggang.luo@amlogic.com>
2012-09-03 12:46:48 +0800

    fix bug: the ape_parser in ffmpeg will give wrong infomation on ape_packet in some case

--- amffmpeg/libavformat/ape.c
+++ amffmpeg/libavformat/ape.c
@@ -156,8 +156,8 @@ static int ape_read_header(AVFormatContext * s, AVFormatParameters * ap)
     AVStream *st;
     uint32_t tag;
     int i;
-    int total_blocks;
-    int64_t pts;
+    int total_blocks, final_size = 0;
+    int64_t pts, file_size;
 
     /* Skip any leading junk such as id3v2 tags */
     ape->junklength = avio_tell(pb);
@@ -283,9 +283,17 @@ static int ape_read_header(AVFormatContext * s, AVFormatParameters * ap)
         ape->frames[i - 1].size = ape->frames[i].pos - ape->frames[i - 1].pos;
         ape->frames[i].skip     = (ape->frames[i].pos - ape->frames[0].pos) & 3;
     }
-    ape->frames[ape->totalframes - 1].size    = ape->finalframeblocks * 4;
+    //ape->frames[ape->totalframes - 1].size    = ape->finalframeblocks * 4;
     ape->frames[ape->totalframes - 1].nblocks = ape->finalframeblocks;
-
+    file_size = avio_size(pb);
+    if (file_size > 0) {
+        final_size = file_size - ape->frames[ape->totalframes - 1].pos -
+                     ape->wavtaillength;
+        final_size -= final_size & 3;
+    }
+    if (file_size <= 0 || final_size <= 0)
+        final_size = ape->finalframeblocks * 8;
+    ape->frames[ape->totalframes - 1].size = final_size;
     for (i = 0; i < ape->totalframes; i++) {
         if(ape->frames[i].skip){
             ape->frames[i].pos  -= ape->frames[i].skip;
@@ -353,11 +361,11 @@ static int ape_read_packet(AVFormatContext * s, AVPacket * pkt)
     if (url_feof(s->pb))
 	return AVERROR_EOF;
         //return AVERROR(EIO);
-    if (ape->currentframe > ape->totalframes)
+    if (ape->currentframe >= ape->totalframes)
 	return AVERROR_EOF;
         //return AVERROR(EIO);
     //find the correct currentframe when seeking
-    while(1)
+    while(0)
     {
         if(ape->frames[ape->currentframe].pos==s->pb->pos)
             break;
