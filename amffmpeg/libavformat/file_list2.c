9605ef8d4460dc784c783e5e8830af4853fe7559
xiaoqiang.zhu <xiaoqiang.zhu@amlogic.com>
2012-09-23 13:32:12 +0800

    [HLS]:fix download item policy,avoid to reduce item failed download

--- amffmpeg/libavformat/file_list2.c
+++ amffmpeg/libavformat/file_list2.c
@@ -448,23 +448,20 @@ static int list_open(URLContext *h, const char *filename, int flags)
         return ret;
     }
     lp_lock_init(&mgt->mutex, NULL);
-    if (!mgt->have_list_end && (!mgt->have_sub_list) && (mgt->target_duration < 5 || mgt->item_num > 10)) {
-        struct list_item *item = mgt->item_list;
-        //int itemindex = mgt->item_num / 2 + 1; /*for live streaming ,choose the middle item.*/
-        int itemindex;
-        if(mgt->item_num > 2)
-            itemindex = mgt->item_num - 2;
-        else
-            itemindex = mgt->item_num/2 + 1;
-        while (--itemindex > 0 && item != NULL) {
-            item = item->next;
-        }
-        mgt->current_item = item;
-    } else {
-        mgt->current_item = mgt->item_list;
-    }
-    mgt->current_item->have_list_end = mgt->have_list_end;
- 
+	
+	float value = 0.0;
+	ret = am_getconfig_float("libplayer.hls.stpos", &value);
+	if (ret < 0 || value <= 0) {	
+		if (!mgt->have_list_end) {			
+			int itemindex = mgt->item_num / 2+1; /*for live streaming ,choose the middle item.*/
+			mgt->current_item = list_find_item_by_index(mgt,itemindex-1);
+		} else {
+			mgt->current_item = mgt->item_list;
+		}		
+	}else{
+		mgt->current_item = list_find_item_by_index(mgt,(int)value-1);
+	}	
+   
     h->is_streamed = 1;
     h->is_slowmedia = 1;
     if (mgt->full_time > 0 && mgt->have_list_end) {
