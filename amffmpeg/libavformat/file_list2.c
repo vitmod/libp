vitmod/libp

commit 3b738901ae8e9369f4640d76a95dc6e4fd5cee15

[HLS]:fix incomplete commits about read more retries for git:af4239c97dcfe7fe5a3dc0314ac1cd265107a151

amffmpeg/libavformat/file_list2.c
@@ -844,6 +844,7 @@ URLProtocol file_list_protocol = {
 list_item_t* getCurrentSegment(void* hSession)
 {
     struct list_item *item = gListMgt->current_item;
+    item->have_list_end = gListMgt->have_list_end;
     return item;
 
 }

commit 288ca8c67d89806726d237bf505622344c58e9a8

[HLS]: fix a bug caused by commit 3b738901ae8e9369f4640d76a95dc6e4fd5cee15

amffmpeg/libavformat/file_list2.c
@@ -844,7 +844,8 @@ URLProtocol file_list_protocol = {
 list_item_t* getCurrentSegment(void* hSession)
 {
     struct list_item *item = gListMgt->current_item;
-    item->have_list_end = gListMgt->have_list_end;
+    if(item)
+        item->have_list_end = gListMgt->have_list_end;
     return item;
 
 }