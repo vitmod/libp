5341fa23b50593b941109d42e10af86707576358
chen.zhang <chen.zhang@amlogic.com>
2012-10-12 20:38:59 +0800

    init frame duration for ts

--- amplayer/player/player_ts.c
+++ amplayer/player/player_ts.c
@@ -33,6 +33,7 @@ static int stream_ts_init(play_para_t *p_para)
             codec->am_sysinfo.format = vinfo->video_codec_type;
             codec->am_sysinfo.width = vinfo->video_width;
             codec->am_sysinfo.height = vinfo->video_height;
+            codec->am_sysinfo.rate = vinfo->video_rate;
             if (p_para->pFormatCtx->pb && p_para->pFormatCtx->pb->is_slowmedia) {
                 /* ts slow media, use idr framerate */
                 log_print("[%s:%d]Slow media detected for ts\n", __FUNCTION__, __LINE__);
