f7087adbfc15e2c4de3dd446981bb43633666433
hui.xu <hui.xu@amlogic.com>
2012-12-24 11:31:20 +0800

    if HDMI_AUTHENTICATE_PATH open failed, return -1, consider hdmi not connected

--- amavutils/Amvideoutils.c
+++ amavutils/Amvideoutils.c
@@ -299,5 +299,16 @@ int amvideo_utils_get_video_angle(int *angle)
 int amvideo_utils_get_hdmi_authenticate(void)
 {
     LOG_FUNCTION_NAME    
-    return amsysfs_get_sysfs_int(HDMI_AUTHENTICATE_PATH);    
+    int fd;
+    int val = 0;
+    char  bcmd[16];
+    fd = open(HDMI_AUTHENTICATE_PATH, O_RDONLY);
+    if (fd >= 0) {
+        read(fd, bcmd, sizeof(bcmd));
+        val = strtol(bcmd, NULL, 10);
+        close(fd);
+    }else {
+        val = -1;
+    }
+    return val;   
 }
