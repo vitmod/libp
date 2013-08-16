e112f14be06fadafa7ff97f2b3ff8d61939d0c94
tellen.yu <tellen.yu@amlogic.com>
2013-08-16 13:35:13 +0800

    fix log function from libutils to liblog

--- amplayer/player/Android.mk
+++ amplayer/player/Android.mk
@@ -63,7 +63,7 @@ LOCAL_CFLAGS+=-DLIBPLAYER_BUILD_NAME=\"${LIBPLAYER_BUILD_NAME}\"
 LOCAL_CFLAGS+=-DLIBPLAYER_GIT_UNCOMMIT_FILE_NUM=${LIBPLAYER_GIT_UNCOMMIT_FILE_NUM}
 
 LOCAL_STATIC_LIBRARIES := libamcodec libavformat librtmp libswscale libavcodec libavutil libamadec
-LOCAL_SHARED_LIBRARIES += libutils libmedia libz libbinder libdl libcutils libc libamavutils libssl libcrypto libamavutils
+LOCAL_SHARED_LIBRARIES += libutils libmedia libz libbinder libdl libcutils libc libamavutils libssl libcrypto
 
 LOCAL_MODULE := libamplayer
 LOCAL_MODULE_TAGS := optional
