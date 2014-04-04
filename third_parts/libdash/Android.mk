198cebc08b39da84cc22e0ca7bc6677a4a8c1b78
le.yang <le.yang@amlogic.com>
2014-04-04 16:26:11 +0800

    PD #90016: To compile the 4.4 version of dash module.            resovle package the empty lib problem.

--- third_parts/libdash/Android.mk
+++ third_parts/libdash/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_IS_HOST_MODULE := true
 
 LOCAL_MODULE_VERSION := $(shell expr substr "$(PLATFORM_VERSION)" 1 3)
-ifneq ($(wildcard libdash_mod_$(LOCAL_MODULE_VERSION).so),)
+ifneq ($(wildcard $(LOCAL_PATH)/libdash_mod_$(LOCAL_MODULE_VERSION).so),)
 LOCAL_SRC_FILES := libdash_mod_$(LOCAL_MODULE_VERSION).so
 else
 LOCAL_SRC_FILES := libdash_mod.so
