f52fd1d2ea38f83f93e7b330b6ec6aa4ed506801
tao.dong <tao.dong@amlogic.com>
2015-01-27 14:53:45 +0800

    Revert " PD #102381(101283): wake mate thread when searching"
    This reverts commit e1ceeabc2eaac583c210e7efef7e4f29b53899c0.

--- amplayer/player/player_para.c
+++ amplayer/player/player_para.c
@@ -1353,9 +1353,7 @@ int player_dec_reset(play_para_t *p_para)
     
 	log_print("player dec reset p_para->playctrl_info.time_point=%f\n",p_para->playctrl_info.time_point);
     if((p_para->playctrl_info.time_point>=0) && ((p_para->state.full_time > 0) || (p_para->start_param->is_livemode == 1))){	
-        player_mate_wake(p_para, 100 * 1000);
-        ret = time_search(p_para,-1);
-        player_mate_sleep(p_para);
+    	 ret = time_search(p_para,-1);
     }else{
         if(p_para->pFormatCtx && p_para->pFormatCtx->pb && p_para->stream_type == STREAM_RM){
             int errorretry = 100;
